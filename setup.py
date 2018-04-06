501dc747b1091054404c18e5feeb65935b900386
Eric Nussbaumer <ebaumer@gmail.com>
2018-04-06 16:22:46 -0600

    Fix install issues

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='standard_precip',
       author='Eric Nussbaumer',
       author_email='ebaumer@gmail.com',
       license='Apache License 2.0',
-      packages=['standard_precip','standard_precip.tests'],
+      packages=['standard_precip'],
       zip_safe=False)
